15ac4c70b59e18dcb888148aeef51d54d0acf006
Marc Mylemans <33804113+marcmylemans@users.noreply.github.com>
2024-10-25 16:03:11 +0200

    Create Update-UserLanguageGroups.ps1

---
+++ Update-UserLanguageGroups.ps1
@@ -0,0 +1,102 @@
+﻿
+<#
+    .SYNOPSIS
+        Create a organised OU Structure for multi client use.
+    .DESCRIPTION
+    Assigns users to language-specific security groups based on the language portion of their preferredLanguage attribute in Active Directory.
+    .NOTES
+        Version:        1.0
+        Author:         Marc Mylemans
+        Creation Date:  25/10/2024
+        Purpose/Change: Initial script development
+    .EXAMPLE
+        .\Update-UserLanguageGroups.ps1
+
+#>
+
+# Import Active Directory module
+Import-Module ActiveDirectory
+
+#-----------------------------Error Action-------------------------------
+
+$ErrorActionPreference= 'silentlycontinue'
+
+#-----------------------------Variables----------------------------------
+# Define log file path
+$logFile = "C:\Temp\Update-UserLanguageGroups.log"
+
+# Define the security groups corresponding to each language policy
+$languageGroups = @{
+    "FR" = "User Policy Language FR"
+    "EN" = "User Policy Language EN"
+    "NL" = "User Policy Language NL"
+}
+
+# Define the distinguished names of the security groups using -Identity
+$groupDNs = @{
+    "FR" = (Get-ADGroup -Identity $languageGroups["FR"]).DistinguishedName
+    "EN" = (Get-ADGroup -Identity $languageGroups["EN"]).DistinguishedName
+    "NL" = (Get-ADGroup -Identity $languageGroups["NL"]).DistinguishedName
+}
+
+# Function to remove user from all language groups
+function Remove-FromAllLanguageGroups {
+    param (
+        [Parameter(Mandatory = $true)]
+        [string]$UserDN
+    )
+    foreach ($groupDN in $groupDNs.Values) {
+        Remove-ADGroupMember -Identity $groupDN -Members $UserDN -ErrorAction SilentlyContinue
+    }
+}
+
+# Get all users with the 'preferredLanguage' attribute
+$users = Get-ADUser -Filter { preferredLanguage -like "*" } -Properties preferredLanguage, DistinguishedName, SamAccountName
+
+foreach ($user in $users) {
+    # Extract the language code from the preferredLanguage attribute (e.g., 'en' from 'en-US')
+    $preferredLanguageFull = $user.preferredLanguage
+    $preferredLanguageCode = $preferredLanguageFull.Split('-')[0].ToUpper()
+    $userDN = $user.DistinguishedName
+    $samAccountName = $user.SamAccountName
+
+    if ($languageGroups.ContainsKey($preferredLanguageCode)) {
+        $targetGroupDN = $groupDNs[$preferredLanguageCode]
+        
+        # Add user to the target group if not already a member
+        if (-not (Get-ADGroupMember -Identity $targetGroupDN -Recursive | Where-Object { $_.DistinguishedName -eq $userDN })) {
+            Add-ADGroupMember -Identity $targetGroupDN -Members $userDN
+            Write-Output "Added $samAccountName to $($languageGroups[$preferredLanguageCode])"
+        }
+
+        # Remove user from other language groups
+        foreach ($lang in $languageGroups.Keys) {
+            if ($lang -ne $preferredLanguageCode) {
+                $otherGroupDN = $groupDNs[$lang]
+                Remove-ADGroupMember -Identity $otherGroupDN -Members $userDN -ErrorAction SilentlyContinue
+                Write-Output "Removed $samAccountName from $($languageGroups[$lang])"
+            }
+        }
+    }
+    else {
+        # If preferredLanguage is not set correctly, remove from all language groups
+        Remove-FromAllLanguageGroups -UserDN $userDN
+        Write-Output "Removed $samAccountName from all language groups due to undefined or unsupported preferredLanguage: $preferredLanguageFull"
+    }
+}
+
+# Function to log messages
+function Log-Message {
+    param (
+        [string]$Message
+    )
+    $timestamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+    "$timestamp - $Message" | Out-File -FilePath $logFile -Append
+}
+
+# Replace Write-Output with Log-Message
+Log-Message "Added $samAccountName to $($languageGroups[$preferredLanguageCode])"
+Log-Message "Removed $samAccountName from $($languageGroups[$lang])"
+Log-Message "Removed $samAccountName from all language groups due to undefined or unsupported preferredLanguage: $preferredLanguageFull"
+Log-Message "Language group update completed at $(Get-Date)"
+
